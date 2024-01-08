0284d7f940007878817373fe154d4a8c13235447
kanishk singhal <72912708+kanix-1801@users.noreply.github.com>
2024-01-08 13:10:26 +0000

    add dataTypes

--- 01_basic_js/index.js
+++ 01_basic_js/index.js
@@ -1,1 +1,42 @@
-console.log("kanishk")
+// const accountId = 144553
+// let email = "kanishk.singhal1801@gmail.com"
+// var accountPassword = "12345"
+// accountCity = "jaipur"
+
+// console.table([accountId , email , accountCity , accountPassword]);
+// console.log("kanishk")
+
+// type conversion 
+// let score = undefined
+// let score = true
+// let score = 12
+// let score = "kanishk"
+// let score = NaN
+// let score = "33"
+let score = null
+const valueInNumber = Number(score)
+// console.log(typeof score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+// conversion in Number
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// undefined => NaN
+
+
+// let isLoggedIn = 12 // --> true
+// let isLoggedIn = 1 // --> true
+let isLoggedIn = 0 // --> false
+let valueInBool = Boolean(isLoggedIn)
+// console.log(typeof isLoggedIn)
+// console.log(typeof valueInBool)
+// console.log(valueInBool)
+
+let someNumber = true
+// let someNumber =
+let stringNum = String(someNumber)
+console.log(typeof someNumber)
+console.log(typeof stringNum)
+console.log(stringNum)
