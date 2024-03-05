e5b43e998c86c446b1433a82da90fe03411694f6
kanishk singhal <72912708+kanix-1801@users.noreply.github.com>
2024-03-05 21:47:05 +0000

    add datatypes

--- 01_basic_js/operations.js
+++ 01_basic_js/operations.js
@@ -23,10 +23,65 @@
 // console.log(+"") //0
 // console.log(-"") //-0
 
-let gameCounter = 100;
-gameCounter++;
-console.log(gameCounter); //101
-console.log(++gameCounter); //102
-console.log(gameCounter++); //102
-console.log(gameCounter); //103
+// let gameCounter = 100;
+// gameCounter++;
+// console.log(gameCounter); //101
+// console.log(++gameCounter); //102
+// console.log(gameCounter++); //102
+// console.log(gameCounter); //103
 
+
+// console.log("2" > 1) //true  
+// console.log("02" > 1) //true  
+// console.log("02" < 1) //false  
+
+// console.log(null >= 0) //true
+// console.log(null < 0) //false
+// console.log(null <= 0) //true
+// console.log(null >= 0) //true
+// console.log(null > 0) //false
+// console.log(null == 0) //false
+
+// ===
+// console.log("2" === 2) //false
+
+
+//datatypes
+
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
