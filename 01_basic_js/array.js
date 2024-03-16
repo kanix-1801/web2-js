221658c3ca7a3d2f9d0c145eef2313588a3ff35e
kanishk singhal <72912708+kanix-1801@users.noreply.github.com>
2024-03-16 04:47:44 +0000

    array

---
+++ 01_basic_js/array.js
@@ -0,0 +1,18 @@
+// array
+// const arr = [1,2,3];
+const arr =[];
+arr[0] = 1;
+arr[1] = 5;
+arr[2] = 8;
+arr[3] = 10; 
+
+// const arr = new Array(1, 2, 3);
+// arr[2] = 10; // also can change the value
+// const arr = new Array("Saab", "Volvo", "BMW");
+
+// console.log(arr[0]);
+// for(let i = 0; i < arr.length;i++){
+//     console.log(arr[i]);
+// }
+console.log(arr);
+
